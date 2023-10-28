03f5de232e2bda5a5724050198e19034d9295d93
amefure <shachi10969@gmail.com>
2023-10-28 21:18:52 +0900

    ローカルへ通知情報を保存

--- app/src/main/java/com/amefure/minnanotanjyoubi/Model/DataStore/DataStoreManager.kt
+++ app/src/main/java/com/amefure/minnanotanjyoubi/Model/DataStore/DataStoreManager.kt
@@ -12,13 +12,18 @@ import java.io.IOException
 class DataStoreManager(private val context: Context) {
 
     companion object {
+        // 「7:00」形式で保存
         val NOTIFY_TIME = stringPreferencesKey("notify_time")
+        // 「当日」or「前日」
+        val NOTIFY_DAY = stringPreferencesKey("notify_day")
+        // 「通知に発行したいメッセージ」
+        val NOTIFY_MESSAGE = stringPreferencesKey("notify_message")
     }
 
-    suspend fun saveNotifyTime(sortItem: String) {
+    suspend fun saveNotifyTime(notifyTime: String) {
         try {
             context.dataStore.edit { preferences ->
-                preferences[NOTIFY_TIME] = sortItem
+                preferences[NOTIFY_TIME] = notifyTime
             }
         } catch (e: IOException) {
             // Handle the exception here if needed
@@ -34,6 +39,50 @@ class DataStoreManager(private val context: Context) {
             }
         }.map { preferences ->
             preferences[NOTIFY_TIME]
+        }
+    }
+
+    suspend fun saveNotifyDay(notifyDay: String) {
+        try {
+            context.dataStore.edit { preferences ->
+                preferences[NOTIFY_DAY] = notifyDay
+            }
+        } catch (e: IOException) {
+            // Handle the exception here if needed
+        }
+    }
+
+    public fun observeNotifyDay(): Flow<String?> {
+        return context.dataStore.data.catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }.map { preferences ->
+            preferences[NOTIFY_DAY]
+        }
+    }
+
+    suspend fun saveNotifyMsg(notifyMsg: String) {
+        try {
+            context.dataStore.edit { preferences ->
+                preferences[NOTIFY_MESSAGE] = notifyMsg
+            }
+        } catch (e: IOException) {
+            // Handle the exception here if needed
+        }
+    }
+
+    public fun observeNotifyMsg(): Flow<String?> {
+        return context.dataStore.data.catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }.map { preferences ->
+            preferences[NOTIFY_MESSAGE]
         }
     }
 }
--- app/src/main/java/com/amefure/minnanotanjyoubi/View/Fragment/SettingFragment.kt
+++ app/src/main/java/com/amefure/minnanotanjyoubi/View/Fragment/SettingFragment.kt
@@ -1,23 +1,39 @@
 package com.amefure.minnanotanjyoubi.View.Fragment
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Switch
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.alpha
+import androidx.lifecycle.lifecycleScope
+import com.amefure.minnanotanjyoubi.Model.DataStore.DataStoreManager
 import com.amefure.minnanotanjyoubi.R
+import kotlinx.coroutines.launch
 import java.util.Calendar
 
 class SettingFragment : Fragment() {
+
+    lateinit var dataStoreManager: DataStoreManager
+
+    private var notifyTime: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        dataStoreManager = DataStoreManager(this.requireContext())
         return inflater.inflate(R.layout.fragment_setting, container, false)
     }
 
@@ -25,7 +41,13 @@ class SettingFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         // Button
         val backButton: ImageButton = view.findViewById(R.id.back_button)
-        val notifySettingTimeButton: ImageButton = view.findViewById(R.id.notify_setting_time_button)
+        val notifyTimeButton: Button = view.findViewById(R.id.notify_setting_time_button)
+        val notifyDayButton: Button = view.findViewById(R.id.notify_setting_day_button)
+        val notifyMsgButton: Button = view.findViewById(R.id.notify_setting_msg_button)
+
+
+        // ローカルに保存している通知情報を取得
+        observeNotifyInfo(view)
 
         // 戻るボタン
         backButton.setOnClickListener {
@@ -33,26 +55,81 @@ class SettingFragment : Fragment() {
             parentFragmentManager.popBackStack()
         }
 
-        notifySettingTimeButton.setOnClickListener {
-//            var year = 0
-//            var month = 0
-//            var day = 0
-//            if (viewModel.selectDate.value != null) {
-//                // 日付を選択済みの場合は初期値を変更
-//                val dateStr = viewModel.selectDate.value!!
-//                val parts = dateStr.split("[年月日]".toRegex())
-//                year = parts[0].toInt()
-//                month = parts[1].toInt() - 1
-//                day = parts[2].toInt()
-//            } else {
-//                // 現在の日付情報を取得
-//                val c = Calendar.getInstance()
-//                year = c.get(Calendar.YEAR)
-//                month = c.get(Calendar.MONTH)
-//                day = c.get(Calendar.DAY_OF_MONTH)
-//            }
-//            val dialog = DatePickerDialog(requireContext(), android.R.style.Theme_Holo_Dialog,dateListener, year, month, day)
-//            dialog.show()
+        notifyTimeButton.setOnClickListener {
+            var hour = 0
+            var minute = 0
+            if (notifyTime != null) {
+                // 日付を選択済みの場合は初期値を変更
+                val parts = notifyTime!!.split(":")
+                hour = parts[0].toInt()
+                minute = parts[1].toInt()
+            } else {
+                // 現在の日付情報を取得
+                val c = Calendar.getInstance()
+                hour = c.get(Calendar.HOUR_OF_DAY)
+                minute = c.get(Calendar.MINUTE)
+
+            }
+            // 取得した日付情報を元にダイアログを生成
+            val dialog = TimePickerDialog(requireContext(), android.R.style.Theme_Holo_Dialog, timeListener, hour, minute,false)
+            dialog.show()
+        }
+
+        notifyDayButton.setOnClickListener {
+            if (notifyDayButton.text == "当日") {
+                notifyDayButton.text = "前日"
+                notifyDayButton.setBackgroundColor(ContextCompat.getColor(this.requireContext(),R.color.thema_blue))
+                lifecycleScope.launch{
+                    dataStoreManager.saveNotifyDay("前日")
+                }
+            } else {
+                notifyDayButton.text = "当日"
+                notifyDayButton.setBackgroundColor(ContextCompat.getColor(this.requireContext(),R.color.thema_red))
+                lifecycleScope.launch{
+                    dataStoreManager.saveNotifyDay("当日")
+                }
+            }
+        }
+        notifyMsgButton.setOnClickListener {
+            val dialog = InputNotifyMsgDialogFragment()
+            dialog.show(parentFragmentManager, "custom")
+        }
+    }
+
+
+    // 日付ピッカーから選択された時に呼ばれるリスナー
+    private var timeListener = TimePickerDialog.OnTimeSetListener { view, hour , minutes ->
+        val time = hour.toString() + ":" + minutes.toString()
+        lifecycleScope.launch{
+            dataStoreManager.saveNotifyTime(time)
+        }
+    }
+
+    // ローカルの通知情報を観測
+    private fun observeNotifyInfo(view: View) {
+        val notifyTimeButton: Button = view.findViewById(R.id.notify_setting_time_button)
+        val notifyDayButton: Button = view.findViewById(R.id.notify_setting_day_button)
+        val notifyEditMsg: TextView = view.findViewById(R.id.notify_setting_edit_msg)
+
+        lifecycleScope.launch{
+            dataStoreManager.observeNotifyTime().collect {
+                if (it != null) {
+                    notifyTimeButton.text = it
+                    notifyTime = it
+                }
+            }
+
+            dataStoreManager.observeNotifyDay().collect {
+                if (it != null) {
+                    notifyDayButton.text = it
+                }
+            }
+
+            dataStoreManager.observeNotifyMsg().collect {
+                if (it != null) {
+                    notifyEditMsg.text = it
+                }
+            }
         }
     }
 
