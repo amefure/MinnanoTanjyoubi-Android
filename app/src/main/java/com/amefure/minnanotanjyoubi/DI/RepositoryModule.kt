7bcfc3173431e6634372dacf3f2a623d5adbbbb7
amefure <shachi10969@gmail.com>
2025-06-25 23:29:58 +0900

    コメント追加

--- app/src/main/java/com/amefure/minnanotanjyoubi/DI/RepositoryModule.kt
+++ app/src/main/java/com/amefure/minnanotanjyoubi/DI/RepositoryModule.kt
@@ -13,11 +13,11 @@ import dagger.hilt.components.SingletonComponent
  * このモジュールはアプリケーション全体（Singletonスコープ）で有効
  * RootRepository（インターフェース）を要求されたときに、RootRepositoryImpl（実装）を注入することを定義
  */
-@Module
-@InstallIn(SingletonComponent::class)
+@Module // Hiltに依存性提供の定義をする場所であることを示す
+@InstallIn(SingletonComponent::class) // アプリ全体で共有する依存性（シングルトン）であることを示す
 object RepositoryModule {
 
-    @Provides
+    @Provides // 対象のインターフェースにどの実態を渡すかを定義する
     fun provideRootRepository(
         impl: RootRepositoryImpl
     ): RootRepository = impl
